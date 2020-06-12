3233382fbab1d39f2d0a9880b47fb578b587eec2
antony-guthiga <toniguthiga93@gmail.com>
2020-06-12 14:36:02 +0300

    Add quote array

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent implements OnInit {
 
+  quotes:Quote[] = [
+    {id:1,quote:'Life is about making an impact, not making an income.',author:'Kevin Kruse',submittedBy:'Tony Guthiga'},
+    {id:2,quote:'Behind every successful person, lies a pack of haters.',author:'Eminem',submittedBy:'King Kaka'},
+    {id:3,quote:'It is not important to win, it is important to make the other guy lose.',author:'Anonymous',submittedBy:'Nonini'},
+  ]
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(){
   }
 
 }
