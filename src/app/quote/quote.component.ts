76100edea3e628c6cdc67fd7ddfd3336ae3ba427
antony-guthiga <toniguthiga93@gmail.com>
2020-06-14 12:42:44 +0300

    Add upvote and downvote to new array quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,20 +9,22 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[] = [
-    {id:1,name:'Life is about making an impact, not making an income.',author:'Kevin Kruse',submittedBy:'Tony Guthiga',date: new Date(2020,8,14)},
-    {id:2,name:'Behind every successful person, lies a pack of haters.',author:'Eminem',submittedBy:'King Kaka',date: new Date(20,6,13)},
-    {id:3,name:'It is not important to win, it is important to make the other guy lose.',author:'Anonymous',submittedBy:'Nonini',date: new Date(2018,3,24)},
+    {id:1,name:'Life is about making an impact, not making an income.',author:'Kevin Kruse',submittedBy:'Tony Guthiga',date: new Date(2020,8,14), upvote: 0,downvote: 0},
+    {id:2,name:'Behind every successful person, lies a pack of haters.',author:'Eminem',submittedBy:'King Kaka',date: new Date(20,6,13),upvote: 0,downvote: 0},
+    {id:3,name:'It is not important to win, it is important to make the other guy lose.',author:'Anonymous',submittedBy:'Nonini',date: new Date(2018,3,24),upvote: 0,downvote: 0},
   ]
 
   addNewQuote(quote){
     let quoteLength = this.quotes.length;
     quote.id = quoteLength+1;
     quote.date = new Date(quote.date);
+    quote.upVote = quoteLength+1;
+    quote.downVote = quoteLength+1;
     this.quotes.push(quote)
   }
 
   showForm: boolean = false
-  
+
   deleteQuote(isComplete, index){
     if (isComplete) {
       let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
@@ -32,6 +34,7 @@ export class QuoteComponent implements OnInit {
       }
     }
   }
+  
   constructor() { }
 
   ngOnInit(){
