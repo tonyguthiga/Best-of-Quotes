862744bcfc2808e0da80b603c0c097e6f2e0b786
antony-guthiga <toniguthiga93@gmail.com>
2020-06-14 09:45:34 +0300

    Add new form, hide and show form

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,10 +10,19 @@ export class QuoteComponent implements OnInit {
 
   quotes:Quote[] = [
     {id:1,name:'Life is about making an impact, not making an income.',author:'Kevin Kruse',submittedBy:'Tony Guthiga',date: new Date(2020,8,14)},
-    {id:2,name:'Behind every successful person, lies a pack of haters.',author:'Eminem',submittedBy:'King Kaka',date: new Date(2019,6,13)},
+    {id:2,name:'Behind every successful person, lies a pack of haters.',author:'Eminem',submittedBy:'King Kaka',date: new Date(20,6,13)},
     {id:3,name:'It is not important to win, it is important to make the other guy lose.',author:'Anonymous',submittedBy:'Nonini',date: new Date(2018,3,24)},
   ]
 
+  addNewQuote(quote){
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength+1;
+    quote.date = new Date(quote.date);
+    this.quotes.push(quote)
+  }
+
+  showForm: boolean = false
+  
   deleteQuote(isComplete, index){
     if (isComplete) {
       let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
@@ -27,5 +36,11 @@ export class QuoteComponent implements OnInit {
 
   ngOnInit(){
   }
-
+  
+  addQuote(){
+    this.showForm = true
+  }
+  hideForm(){
+    this.showForm = false
+  }
 }
