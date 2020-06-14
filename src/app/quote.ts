a16c552f12c619e32880d2053447b2c312779353
antony-guthiga <toniguthiga93@gmail.com>
2020-06-14 12:39:34 +0300

    Add upvote and downvote to class quote constructor

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,4 +1,4 @@
 export class Quote {
-    constructor(public id: number,public name: string,public author: string,public submittedBy: string,public date: Date) {
+    constructor(public id: number,public name: string,public author: string,public submittedBy: string,public date: Date,public upvote: number,public downvote: number) {
     }
 }
