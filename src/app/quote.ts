0395aa46f25ebe8102495cc3705ed6b0ea34558d
antony-guthiga <toniguthiga93@gmail.com>
2020-06-16 14:02:26 +0300

    Equate upvote and downvote to zero

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,4 +1,8 @@
 export class Quote {
-    constructor(public id: number,public name: string,public author: string,public submittedBy: string,public date: Date,public upvote: number,public downvote: number) {
+    public upVotes: number;
+    public downVotes: number;
+    constructor(public id: number,public name: string,public author: string,public submittedBy: string,public date: Date) {
+        this.upVotes = 0;
+        this.downVotes = 0;
     }
 }
